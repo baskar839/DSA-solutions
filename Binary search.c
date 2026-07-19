971d81534908df45ac89ba33380b056ca8d1367f
baskar839 <baskarrajendran4@gmail.com>
2026-07-19 22:07:47 +0530

    Create Binary search.c

---
+++ Binary search.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+int main() {
+    int arr[] = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
+    int n = sizeof(arr) / sizeof(arr[0]);
+    int key = 70;
+
+    int low = 0;
+    int high = n - 1;
+    int mid;
+    int found = 0;
+
+    while (low <= high) {
+        mid = (low + high) / 2;
+
+        if (arr[mid] == key) {
+            printf("Element %d found at position %d.\n", key, mid + 1);
+            found = 1;
+            break;
+        }
+        else if (arr[mid] < key) {
+            low = mid + 1;
+        }
+        else {
+            high = mid - 1;
+        }
+    }
+
+    if (found == 0) {
+        printf("Element %d not found in the array.\n", key);
+    }
+
+    return 0;
+}
